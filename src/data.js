422a420e4688482c581eaa9bfefaafd72a1d28e1
Charly4444 <Charleydynamic@gmail.com>
2023-03-03 20:52:01 +0100

    initial

--- src/data.js
+++ src/data.js
@@ -1,61 +1,52 @@
-// tour data
+const personData = [
+    {
+        id: 1,
+        name: 'tommy',
+        title: 'full stack web developer',
+        date: 'December 2015 - Present',
+        desc: [
+            `Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed 
+            brunch neutra. Hammock photo booth live-edge disrupt.`,
+            `Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro 
+            tattooed copper mug. Meditation lomo biodiesel scenester`,
+            `Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica 
+            fixie church-key, small batch jianbing messenger bag scenester +1`,
+        ],
+        klass: 'job-btn'
+    },
+    {
+        id: 2,
+        name: 'bigdrop',
+        title: 'front-end engineer',
+        date: 'May 2015 - December 2015',
+        desc: [
+            `Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. 
+            Cardigan church-key pabst, biodiesel vexillologist viral squid.`,
+            `Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro 
+            tattooed copper mug. Meditation lomo biodiesel scenester`,
+            `Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica 
+            fixie church-key, small batch jianbing messenger bag scenester +1`,
+            `Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica 
+            fixie church-key, small batch jianbing messenger bag scenester +1`,
+        ],
+        klass: 'job-btn'
+    },
+    {
+        id: 3,
+        name: 'cuker',
+        title: 'engineering intern',
+        date: 'May 2014 - september 2015',
+        desc: [
+            `Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. 
+            Cardigan church-key pabst, biodiesel vexillologist viral squid.`,
+            `YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix 
+            unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic 
+            pug ugh taxidermy squid.`,
+            `Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica 
+            fixie church-key, small batch jianbing messenger bag scenester +1`,
+        ],
+        klass: 'job-btn'
+    },
+]
 
-const tour = [
-    {
-        id:1,
-        price: '$1850',
-        caption: 'Best of Paris in 7 Days Tour',
-        about: `Paris is synonymous with the finest things that culture can offer — in art, fashion, 
-                food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will 
-                immerse you in the very best of the City of Light: the masterpiece-packed Louvre and 
-                Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant 
-                Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic 
-                heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful 
-                markets, and joie de vivre. Join us for the Best of Paris in 7 Days!`,
-    },
-    {
-        id:2,
-        price: '$2550',
-        caption: 'Best of Ireland in 14 Days Tour',
-        about: `Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's 
-                must-see historical sites, charming towns, music-filled pubs, and seaside getaways — including Kinsale,
-                the Dingle Peninsula, the Cliffs of Moher, the Aran Islands, Galway, Connemara, Giant's Causeway, 
-                and the compelling city of Belfast. All along the way, Rick's guides will share their stories to draw 
-                you in to the Emerald Isle, and the friendliness of the people will surely steal your heart. 
-                Join us for the Best of Ireland in 14 Days!`,
-    },
-    {
-        id:3,
-        price: '$1080',
-        caption: 'Best of Salzburg & Vienna in 8 Days Tour',
-        about: `Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you 
-                to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. 
-                Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich, 
-                Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna — 
-                with cozy villages and alpine vistas all along the way. Join us for the Best of Munich, Salzburg 
-                & Vienna in 8 Days!`,
-    },
-    {
-        id:4,
-        price: '$1150',
-        caption: 'Best of Salzburg & Vienna in 8 Days Tour',
-        about: `Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to 
-                the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. Your Rick Steves guide will 
-                bring this region's rich history and culture to life in festive Munich, Baroque Salzburg, 
-                sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna — with cozy villages 
-                and alpine vistas all along the way. Join us for the Best of Munich, Salzburg & Vienna in 8 Days!`,
-    },
-    {
-        id:5,
-        price: '$850',
-        caption: 'Best of Rome in 7 Days Tour',
-        about: `Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, 
-                and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of 
-                ancient Rome's Colosseum, Forum, Pantheon, and nearby Ostia Antica. From the Renaissance and 
-                Baroque eras, you'll marvel at St. Peter's Basilica, the Vatican Museums, Sistine Chapel, 
-                and Borghese Gallery. You'll also enjoy today's Rome, with neighborhood walking tours, 
-                memorable restaurants, and time to explore on your own. Join us for the Best of Rome in 7 Days!`,
-    },
-];
-
-export default tour;
+export default personData;
---
+++ src/tabsApp.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import data from "./data"
+
+
+const TabsApp = () => {
+    var idx = -1;   // some random variable to use for my key, quickfix
+    const defaultPerson = {...data[0], klass:'job-btn active-btn'};
+    const [person, setPerson] = useState(defaultPerson);
+
+    const handleClick = (id)=> {
+        const updatePerson = data.filter((itm) => itm.id === id);
+        // destructuring the object 'updatePerson[0]'
+        setPerson({...updatePerson[0], klass: 'job-btn active-btn'})
+    }
+
+    return <>
+        <section className="section">
+            <div className="title">
+                <h2>experience</h2>
+                <div className="underline"></div>
+            </div>
+            <div className="jobs-center">
+                <div className="btn-container">
+                    {/* added a className of active-btn when clicked */
+                    console.log(person)}
+                    {data.map((individual)=> {
+                        if(individual.id === person.id){individual=person}
+                        return <button key={individual.id} className={individual.klass} onClick={()=>{handleClick(individual.id)}}>
+                            {individual.name}
+                        </button>
+                    })}
+                </div>
+
+                <article className="job-info">
+                    <h3>{person.title}</h3>
+                    <h4>{person.name}</h4>
+                    <p className="job-date">{person.date}</p>
+                    {person.desc.map((item)=>{
+                        return <div key={idx++} className="job-desc">
+                            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" className="job-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"></path>
+                            </svg>
+                            <p>{item}</p>
+                        </div>
+                    })}
+                    
+                </article>
+            </div>
+            <button type="button" className="btn">more info</button>
+        </section>
+    </>
+}
+// this way seems longer, javaScript did all the work
+export default TabsApp;
